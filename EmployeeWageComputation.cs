931dcdaec3eaae6e81a380640ecec4a31477f309
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-10 15:45:18 +0530

    Employee Daily Wage With Part Time

--- EmployeeWageComputation.cs
+++ EmployeeWageComputation.cs
@@ -5,7 +5,8 @@ namespace EmployeeWageComputation
     class Attendance
     {
         //constants
-        public const int IS_PRESENT = 1;
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HR = 20;
 
         static void Main(string[] args)
@@ -16,18 +17,23 @@ namespace EmployeeWageComputation
 
             //creating object 
             Random Attendance = new Random();
-            int userInput = Attendance.Next(0, 2);
+            int userInput = Attendance.Next(0, 3);
 
-            //if else programming construct
-            if(userInput == IS_PRESENT)
+            //if else if programming construct
+            if(userInput == IS_FULL_TIME  )
             {
                 empHr = 8;
-               
+                Console.WriteLine("Employee is Full time");
             }
-            else
+            else if (userInput == IS_PART_TIME )
             {
+                Console.WriteLine("Employee is Part time");
+                empHr = 4;
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
                 empHr = 0;
-
             }
             // Formula for finding employe wage
             empWage = EMP_RATE_PER_HR * empHr;
