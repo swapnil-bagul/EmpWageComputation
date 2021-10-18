6c072a3be9aba36f7712ae3fd416abdb3edf095b
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-18 18:00:04 +0530

    [Swapnil] Added Ability to manage employee wage for multiple company

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class CompanyEmpWage
+    {
+        public int empRate;
+        public int maxWorkingDay;
+        public int maxWorkingHrs;
+        public int totalEmpWage;
+        public string companyName;
+
+        public CompanyEmpWage (string companyName, int maxWorkingDay, int maxWorkingHrs, int empRate)
+        {
+            this.companyName = companyName;
+            this.maxWorkingDay = maxWorkingDay;
+            this.maxWorkingHrs = maxWorkingHrs;
+            this.empRate = empRate;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+
+        }
+        public string toString()
+        {
+            return "Total Employee Wage For " + companyName + " " + "is " + totalEmpWage;
+        }
+    }
+  
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder .addCompanyEmpWage("Tata Motors", 24, 120, 30);
+            empWageBuilder .addCompanyEmpWage("Maruti Suzuki", 25, 110, 35);
+            empWageBuilder .addCompanyEmpWage ("Mahindra",23,100,40);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
